82da99d4d1dd369533e7032827fedd46751d2b84
mpassion <hi@mpassion.dev>
2026-02-12 01:27:30 +0100

    unblockd 0.9.1

--- Casks/unblockd.rb
+++ Casks/unblockd.rb
@@ -1,10 +1,10 @@
 cask "unblockd" do
-  version "0.9.0"
-  sha256 "a9481c5b137b301b373b064c8a29b06adeb8c6c7848f16e64f0bdc6e0afea445"
+  version "0.9.1"
+  sha256 "6d2d6544cf7a7f4441c1adaab0932ffb47bfae090faeb00c09d59706e609d2c0"
 
   url "https://github.com/mpassion/unblockd/releases/download/v#{version}/Unblockd-#{version}.zip"
   name "Unblockd"
-  desc "Menu bar pull request monitor for Bitbucket, GitHub, and GitLab"
+  desc "macOS menu bar app for tracking pull requests"
   homepage "https://github.com/mpassion/unblockd"
 
   app "Unblockd.app"
