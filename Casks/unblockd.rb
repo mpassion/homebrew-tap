edd504ff214058b4deeb62b55989328b1502c730
mpassion <hi@mpassion.dev>
2026-02-14 17:23:25 +0100

    unblockd 0.9.3

--- Casks/unblockd.rb
+++ Casks/unblockd.rb
@@ -1,6 +1,6 @@
 cask "unblockd" do
-  version "0.9.2"
-  sha256 "e2b4f25adeba77cdff6708cd3dcb44816e3a34e16b4efc64bf022b1b96298efa"
+  version "0.9.3"
+  sha256 "65fc49b676db20f92db10216081282e76f32f341246f108e99d401afcd80dfb2"
 
   url "https://github.com/mpassion/unblockd/releases/download/v#{version}/Unblockd-#{version}.zip"
   name "Unblockd"
